ed011cb384da8f99b831ef39c9075e1b862ac55b
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-02-28 10:58:04 +0200

    Added Recipe Request

---
+++ Dev_Pod/Classes/RecipeRequest.swift
@@ -0,0 +1,45 @@
+//
+//  RecipeRequest.swift
+//  Dev_Pod
+//
+//  Created by Akua Afrane-Okese on 2020/02/28.
+//
+
+import Foundation
+
+
+struct FoodRequest {
+    var foodQuery: String
+
+    let defaultSession = URLSession(configuration: .default)
+
+    var dataTask: URLSessionDataTask?
+
+    func getSearchResult(completionHandler: @escaping([FoodDetails]) -> Void) {
+       dataTask?.cancel()
+
+        let resourceString = "http://www.recipepuppy.com/api/?"
+        let resourceQuery = "i=\(foodQuery)"
+
+        guard let resourceURL = URL(string: resourceString + resourceQuery) else {fatalError()}
+        let dataTask = URLSession.shared.dataTask(with: resourceURL) {data, response, error in
+
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200 {
+                print(data)
+
+                do {
+                    let foodList = try JSONDecoder().decode(FoodResults.self, from: data)
+
+                    completionHandler(foodList.resultCount)
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+}
