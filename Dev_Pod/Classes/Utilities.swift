a53ccc57a4796cff0fbd782ddd03dc97a38ff7ac
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-02-28 11:34:28 +0200

    Publicising Utilities Class

--- Dev_Pod/Classes/Utilities.swift
+++ Dev_Pod/Classes/Utilities.swift
@@ -9,23 +9,23 @@ import Foundation
 import UIKit
 
 public class Utilities {
-    static func styleTextField(_ textfield: UITextField) {
+    public static func styleTextField(_ textfield: UITextField) {
         // Remove border on text field
         textfield.layer.borderWidth = 1
         textfield.layer.borderColor = UIColor.init(red: 200/255, green: 135/255, blue: 215/255, alpha: 1).cgColor
         textfield.layer.cornerRadius = 6
     }
-    static func styleFilledButton(_ button: UIButton) {
+    public static func styleFilledButton(_ button: UIButton) {
         // Filled rounded corner style
         button.layer.cornerRadius = 6
     }
-    static func styleHollowButton(_ button: UIButton) {
+    public static func styleHollowButton(_ button: UIButton) {
         // Hollow rounded corner style
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.init(red: 200/255, green: 135/255, blue: 215/255, alpha: 1).cgColor
         button.layer.cornerRadius = 6
     }
-    static func styleErrorTextField(_ textfield: UITextField) {
+    public static func styleErrorTextField(_ textfield: UITextField) {
         // Hollow rounded corner style
         textfield.layer.borderWidth = 1
         textfield.layer.borderColor = UIColor.red.cgColor
