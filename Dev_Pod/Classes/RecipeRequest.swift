29e37197c1600aa89332dbb19488c3940b5eef0b
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-03-22 17:24:42 +0200

    Update to Recipe List Search
    To reduce redundant code, the service was restructured

--- Dev_Pod/Classes/RecipeRequest.swift
+++ Dev_Pod/Classes/RecipeRequest.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public struct FoodRequest {
     public var foodQuery: String
+    public var typeQuery: String
 
     public let defaultSession = URLSession(configuration: .default)
 
@@ -19,7 +20,7 @@ public struct FoodRequest {
 
         let resourceString = "http://www.recipepuppy.com/api/?"
         let countSpaces = foodQuery.replacingOccurrences(of: " ", with: "+")
-        let resourceQuery = "i=\(countSpaces)"
+        let resourceQuery = "\(typeQuery)=\(countSpaces)"
 
         guard let resourceURL = URL(string: resourceString + resourceQuery) else {fatalError()}
         let dataTask = URLSession.shared.dataTask(with: resourceURL) {data, response, error in
@@ -43,36 +44,8 @@ public struct FoodRequest {
         dataTask.resume()
     }
 
-    public func getRecipeResult(completionHandler: @escaping([FoodDetails]) -> Void) {
-       dataTask?.cancel()
-
-        let resourceString = "http://www.recipepuppy.com/api/?"
-        let countSpaces = foodQuery.replacingOccurrences(of: " ", with: "+")
-        let resourceQuery = "q=\(countSpaces)"
-
-        guard let resourceURL = URL(string: resourceString + resourceQuery) else {fatalError()}
-        let dataTask = URLSession.shared.dataTask(with: resourceURL) {data, response, error in
-
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data,
-                let response = response as? HTTPURLResponse,
-                response.statusCode == 200 {
-                print(data)
-
-                do {
-                    let foodList = try JSONDecoder().decode(FoodResults.self, from: data)
-
-                    completionHandler(foodList.resultCount)
-                } catch let error {
-                    print(error)
-                }
-            }
-        }
-        dataTask.resume()
-    }
-
-    public init(foodQuery: String) {
+    public init(foodQuery: String, typeQuery: String) {
         self.foodQuery = foodQuery
+        self.typeQuery = typeQuery
     }
 }
