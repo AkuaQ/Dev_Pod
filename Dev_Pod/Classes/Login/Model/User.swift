7153c8b8c51b62e34ce2df2ac472913ea6dc7268
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-03-27 21:04:13 +0200

    Addition of Login to Dev_Pods
    With Firebase working, login can go to the dev_pod

---
+++ Dev_Pod/Classes/Login/Model/User.swift
@@ -0,0 +1,72 @@
+//
+//  User.swift
+//  Dev_Pod
+//
+//  Created by Akua Afrane-Okese on 2020/03/27.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+protocol UserModelProtocol {
+    func addUser(with firstName: String, lastName: String,
+                 email: String, and password: String, complete: @escaping ( _ error: String) -> Void)
+    func loginUser(with email: String, and password: String,
+                   complete: @escaping ( _ error: String) -> Void)
+}
+
+struct User: UserModelProtocol {
+    public var firstName: String
+    public var lastName: String
+    public var email: String
+    public var password: String
+
+    public func getEmail() -> String {
+        return self.email
+    }
+
+    public func addUser(with firstName: String, lastName: String, email: String,
+                 and password: String, complete: @escaping ( _ error: String) -> Void) {
+        var errorMessage = ""
+        Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+        //Check for errors
+            if err != nil {
+                //There was an error creating the user
+                errorMessage = "Error creating user"
+                complete(errorMessage)
+            } else {
+                //User was created successfully, now store the first name and last name
+                let database = Firestore.firestore()
+                database.collection("users").addDocument(data: ["firstname": firstName,
+                                                                "lastname": lastName,
+                                                                "uid": result!.user.uid]) { (error) in
+                    //if not nil then there was an error with creating first name and last name
+                    if error != nil {
+                        //Show error message
+                        errorMessage = error!.localizedDescription
+                        complete(errorMessage)
+                    } else {
+                        errorMessage = ""
+                        complete(errorMessage)
+                    }
+                }
+            }
+        }
+    }
+
+    public func loginUser(with email: String, and password: String, complete: @escaping ( _ error: String) -> Void) {
+        var errorMessage = ""
+        Auth.auth().signIn(withEmail: email, password: password) { ( _, error) in
+            //Check for errors
+                if error != nil {
+                    //Cannot sign in
+                    errorMessage = error!.localizedDescription
+                    complete(errorMessage)
+                } else {
+                    errorMessage = ""
+                    complete(errorMessage)
+            }
+        }
+    }
+}
+
